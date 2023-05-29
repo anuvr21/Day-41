a16386c5990210a909b8fca598dd7901410732d1
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-29 09:23:19 +0530

    Coding Challenges

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBook book = new MyBook();
+            Console.WriteLine("Enter the book name:");
+            string s = Console.ReadLine();
+               book.SetTitle(s);
+            Console.WriteLine("The book title is: " + book.GetTitle());
+        }
+    }
+    abstract class Book
+    {
+        protected string title;
+        public abstract void SetTitle(string s);
+        public string GetTitle()
+        {
+            return title;
+        }
+    }
+    class MyBook : Book
+    {
+        public override void SetTitle(string s)
+        {
+            title = s;
+        }
+    }
+}
+
---
+++ AnagramsDemo/AnagramsDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramsDemo
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter the first string:");
+
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Enter the second string");
+
+            string b = Console.ReadLine();
+
+            Console.WriteLine(IsAnagram(a, b) ? "Anagram" : "Not Anagram");
+
+        }
+
+        static bool IsAnagram(string a, string b)
+
+        {
+
+            if (a.Length != b.Length)
+
+            {
+
+                return false;
+
+            }
+
+            char[] c1 = a.ToLower().ToCharArray();
+
+            char[] c2 = b.ToLower().ToCharArray();
+
+            Array.Sort(c1);
+
+            Array.Sort(c2);
+
+            for (int i = 0; i < c1.Length || i < c2.Length; i++)
+
+            {
+
+                if (c1[i] != c2[i]) { return false; }
+
+            }
+
+            return true;
+
+        }
+
+    }
+
+}
+
